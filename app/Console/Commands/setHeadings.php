ae2db07bb6a6c70631c8a1d937a7f4ad9ff35ed2
Selk-bit <selkellouti233@gmail.com>
2022-08-27 19:18:43 +0100

    sgarhsre

---
+++ app/Console/Commands/setHeadings.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Article;
+
+
+class setHeadings extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'set:heading';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $latest_articles = Article::query()->select("id")->orderBy("id")->limit("10")->pluck("id");
+        Article::query()->where("heading", 1)->whereNotIn("id", $latest_articles)->update([
+            "heading" => 0
+        ]);
+        Article::query()->whereIn("id", $latest_articles)->update([
+            "heading" => 1
+        ]);
+    }
+}
