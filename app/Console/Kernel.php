778c6aea77795f3bf33c83b5dbc5670dbfaf25db
Selk-bit <selkellouti233@gmail.com>
2022-08-27 20:13:13 +0100

    sdgerher

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,9 +13,14 @@ class Kernel extends ConsoleKernel
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      * @return void
      */
+
+    protected $commands = [
+        'App\Console\Commands\setHeadings',
+    ];
+
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('set:heading')->everyFourHours();
     }
 
     /**
