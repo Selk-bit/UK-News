882fc96964a6f8238bbbb28d5b3ff868c1ad329a
Selk-bit <selkellouti233@gmail.com>
2022-08-28 18:34:52 +0100

    sregerg

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -23,7 +23,6 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('set:heading')->everyFourHours();
-        //$schedule->command('set:popular')->everyFourHours();
 
 
     /**
