836a36582ae3f7f3df0394c8c4c30b2768cdfaec
Selk-bit <selkellouti233@gmail.com>
2022-09-03 14:20:47 +0100

    srgljsjerg

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -162,6 +162,15 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js" integrity="sha512-XtmMtDEcNz2j7ekrtHvOVR4iwwaD6o/FUJe6+Zq+HgcCsk3kj4uSQQR8weQ2QVj1o0Pk6PwYLohm206ZzNfubg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script  src="{{ asset('js/app.js') }}"></script>
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-HQL4E2W0E9"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'G-HQL4E2W0E9');
+	</script>
 </body>
 
 </html>
